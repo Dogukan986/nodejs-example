1c2f5311cd859d2f249ab8a9a87200781283a007
Dogukan986 <dogubektas99@gmail.com>
2024-01-14 17:07:03 +0300

    response structure

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,16 @@
 const userModel = require("../models/user.model")
 const bcrypt = require("bcrypt")
 const APIError = require("../utils/errors")
+const Response = require("../utils/response")
 
 const login = async (req, res) => {
-    console.log("login", req.body)
     return res.json(req.body)
 }
 
 const register = async (req, res) => {
 
     const { email } = req.body
-    const userCheck = await userModel.find({ email: email })
+    const userCheck = await userModel.findOne({ email: email })
 
     if (userCheck) {
         throw new APIError("Girilen mail kullanımda.", 401)
@@ -18,24 +18,15 @@ const register = async (req, res) => {
 
     req.body.password = await bcrypt.hash(req.body.password, 10)
 
-    try {
+    const newUserData = new userModel(req.body)
+    await newUserData.save()
+        .then((res) => {
+            return new Response(res, "Kullanıcı kaydı başarıyla oluştu.").created(res)
+        })
+        .catch((err) => {
+            throw new APIError("Kullanıcı kayıt edilemedi !", 400)
+        })
 
-        const newUserData = new userModel(req.body)
-        const savedUser = await newUserData.save();
-        return res.status(201).json({
-            success: true,
-            data: savedUser,
-            message: "Kayıt başarılı"
-        });
-
-    } catch (err) {
-
-        return res.status(500).json({
-            success: false,
-            error: "Internal Server Error"
-        });
-
-    }
 }
 
 module.exports = { login, register }
