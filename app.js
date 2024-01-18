5eae1c468f578d17f820605db990aa715a991c91
Dogukan986 <dogubektas99@gmail.com>
2024-01-19 00:11:44 +0300

    api injection

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 const port = process.env.PORT || 5001
 const router = require("./src/routers")
 const errorHandleMiddleware = require("./src/middelwares/errorHandler")
+const mongoSanitize = require('express-mongo-sanitize');
 const cors = require("cors")
 const corsOptions = {
     origin: 'http://localhost:5002',
@@ -21,6 +22,12 @@ app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
 
 app.use(cors(corsOptions));
+app.use(
+    mongoSanitize({
+        replaceWith: '_',
+    }),
+);
+
 app.use("/api", router)
 
 //Hata yakalama
