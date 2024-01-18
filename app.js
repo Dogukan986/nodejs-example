d3c86b78e3548d3eab942354a56117c7301d2d11
Dogukan986 <dogubektas99@gmail.com>
2024-01-18 23:56:58 +0300

    cors options

--- app.js
+++ app.js
@@ -7,13 +7,20 @@ const app = express()
 const port = process.env.PORT || 5001
 const router = require("./src/routers")
 const errorHandleMiddleware = require("./src/middelwares/errorHandler")
+const cors = require("cors")
+const corsOptions = {
+    origin: 'http://localhost:5002',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+};
+
 
 //middleware
 app.use(express.json())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
 
-
+app.use(cors(corsOptions));
 app.use("/api", router)
 
 //Hata yakalama
